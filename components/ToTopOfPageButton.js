3061b50d8351e6340f145ecb4d2aa12a9fd2cef5
hzlnutspread <smithkg293@gmail.com>
2022-10-07 20:41:45 +1300

    Change totopofpagebutton styling

--- components/ToTopOfPageButton.js
+++ components/ToTopOfPageButton.js
@@ -1,14 +1,14 @@
-import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import { FiArrowUp } from "react-icons/fi";
 
 const ToTopOfPageButton = () => {
   return (
     <button
-      className="m-2 flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-r from-purple-400 to-blue-500 md:absolute md:right-16 lg:right-20"
+      className="m-2 flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-r from-purple-400 to-blue-500 transition duration-200 ease-in-out hover:scale-[1.2] md:absolute md:right-16 lg:right-20"
       onClick={() => {
         window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
       }}
     >
-      <BsFillArrowUpCircleFill className="h-full w-full transition duration-200 ease-in-out hover:scale-[1.2]" />
+      <FiArrowUp className="h-5 w-5" />
     </button>
   );
 };
